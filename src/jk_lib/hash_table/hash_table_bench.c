75e2154c93ffd4c9a1dc499e1263cf46f4024e95
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2023-10-17 22:12:43 -0400

    Improve JkHashTable and unordered_map benchmark programs

--- src/jk_lib/hash_table/hash_table_bench.c
+++ src/jk_lib/hash_table/hash_table_bench.c
@@ -4,18 +4,66 @@
 #include <time.h>
 
 #define NUM_ELEMENTS 50000000
+#define SEED 1608690770
 
 int main(void)
 {
     JkHashTable *t = jk_hash_table_create();
+    int sum = 0;
+    clock_t start_time;
+    clock_t end_time;
 
-    clock_t start_time = clock();
+    srand(SEED);
+    start_time = clock();
     for (int i = 0; i < NUM_ELEMENTS; i++) {
-        (*jk_hash_table_get_with_default(t, rand(), 0))++;
+        jk_hash_table_put(t, rand(), i & 0xf);
     }
-    clock_t end_time = clock();
+    end_time = clock();
+    printf("Writes w/ resize: %.2f\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
 
-    printf("%.2f\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+    srand(SEED);
+    start_time = clock();
+    for (int i = 0; i < NUM_ELEMENTS; i++) {
+        sum += *jk_hash_table_get(t, rand());
+    }
+    end_time = clock();
+    printf("Reads: %.2f\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+
+    start_time = clock();
+    for (int i = 0; i < NUM_ELEMENTS; i++) {
+        JkHashTableValue *value = jk_hash_table_get(t, rand());
+        if (value) {
+            sum += *value;
+        }
+    }
+    end_time = clock();
+    printf("Misses: %.2f\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+
+    srand(SEED);
+    start_time = clock();
+    for (int i = 0; i < NUM_ELEMENTS; i++) {
+        jk_hash_table_remove(t, rand());
+    }
+    end_time = clock();
+    printf("Removes: %.2f\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+
+    srand(SEED);
+    start_time = clock();
+    for (int i = 0; i < NUM_ELEMENTS; i++) {
+        jk_hash_table_put(t, rand(), i & 0xf);
+    }
+    end_time = clock();
+    printf("Writes: %.2f\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+
+    // Somehow prevents a compiler "optimization" from slowing down the reads timing
+    for (int i = 0; i < 10; i++) {
+        JkHashTableValue *value = jk_hash_table_get(t, rand());
+        if (value) {
+            sum += *value;
+        }
+    }
+
+    printf("Sum: %d\n", sum);
 
     jk_hash_table_destroy(t);
 
--- src/learning_cpp/unordered_map_bench.cpp
+++ src/learning_cpp/unordered_map_bench.cpp
@@ -6,18 +6,66 @@
 using namespace std;
 
 constexpr int NUM_ELEMENTS = 50'000'000;
+constexpr int SEED = 1608690770;
 
 int main()
 {
     unordered_map<int, int> map;
+    int sum = 0;
+    clock_t start_time;
+    clock_t end_time;
 
-    clock_t start_time = clock();
+    srand(SEED);
+    start_time = clock();
     for (int i = 0; i < NUM_ELEMENTS; i++) {
-        map[rand()]++;
+        map[rand()] = i & 0xf;
     }
-    clock_t end_time = clock();
+    end_time = clock();
+    printf("Writes w/ resize: %.2f\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
 
-    printf("%.2f\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+    srand(SEED);
+    start_time = clock();
+    for (int i = 0; i < NUM_ELEMENTS; i++) {
+        sum += map[rand()];
+    }
+    end_time = clock();
+    printf("Reads: %.2f\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+
+    start_time = clock();
+    for (int i = 0; i < NUM_ELEMENTS; i++) {
+        int num = rand();
+        if (map.contains(num)) {
+            sum += map[num];
+        }
+    }
+    end_time = clock();
+    printf("Misses: %.2f\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+
+    srand(SEED);
+    start_time = clock();
+    for (int i = 0; i < NUM_ELEMENTS; i++) {
+        map.erase(rand());
+    }
+    end_time = clock();
+    printf("Removes: %.2f\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+
+    srand(SEED);
+    start_time = clock();
+    for (int i = 0; i < NUM_ELEMENTS; i++) {
+        map[rand()] = i & 0xf;
+    }
+    end_time = clock();
+    printf("Writes: %.2f\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+
+    // Somehow prevents a compiler "optimization" from slowing down the reads timing
+    for (int i = 0; i < 10; i++) {
+        int num = rand();
+        if (map.contains(num)) {
+            sum += map[num];
+        }
+    }
+
+    printf("Sum: %d\n", sum);
 
     return 0;
 }
