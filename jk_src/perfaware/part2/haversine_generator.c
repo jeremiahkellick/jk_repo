81f973678aee7b3db69af8f462e16c98e90ae86e
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2024-02-19 10:52:37 -0500

    Implement haversine_generator

--- jk_src/perfaware/part2/haversine_generator.c
+++ jk_src/perfaware/part2/haversine_generator.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,12 +9,17 @@
 #include <jk_src/jk_lib/command_line/options.c>
 #include <jk_src/jk_lib/hash.c>
 
-#define DEFAULT_FILE_PATH "./coords.json"
+#define DEFAULT_JSON_FILE_PATH "./coords.json"
+#define DEFAULT_ANSWER_FILE_PATH "./answers.f64"
 
 #define DEFAULT_PAIR_COUNT 1000
 #define DEFAULT_PAIR_COUNT_STRING "1,000"
 #define MAX_PAIR_COUNT 1000000000
 #define MAX_PAIR_COUNT_STRING "1,000,000,000"
+
+#define EARTH_RADIUS 6372.8
+#define PI 3.14159265358979323846264338327950288
+#define DEGREES_TO_RADIANS (PI / 180.0)
 
 char *program_name = "<program_name global should be overwritten with argv[0]>";
 
@@ -33,6 +39,49 @@ static unsigned hash_string(char *string)
         hash = jk_hash_uint32(hash ^ bits.u);
     }
     return hash;
+}
+
+static double random_between(double min, double max)
+{
+    return min + ((double)rand() / (double)RAND_MAX) * (max - min);
+}
+
+static double square(double A)
+{
+    double Result = (A * A);
+    return Result;
+}
+
+static double radians_from_degrees(double Degrees)
+{
+    double Result = 0.01745329251994329577 * Degrees;
+    return Result;
+}
+
+// NOTE(casey): EarthRadius is generally expected to be 6372.8
+static double reference_haversine(double X0, double Y0, double X1, double Y1, double EarthRadius)
+{
+    /* NOTE(casey): This is not meant to be a "good" way to calculate the Haversine distance.
+       Instead, it attempts to follow, as closely as possible, the formula used in the real-world
+       question on which these homework exercises are loosely based.
+    */
+
+    double lat1 = Y0;
+    double lat2 = Y1;
+    double lon1 = X0;
+    double lon2 = X1;
+
+    double dLat = radians_from_degrees(lat2 - lat1);
+    double dLon = radians_from_degrees(lon2 - lon1);
+    lat1 = radians_from_degrees(lat1);
+    lat2 = radians_from_degrees(lat2);
+
+    double a = square(sin(dLat / 2.0)) + cos(lat1) * cos(lat2) * square(sin(dLon / 2));
+    double c = 2.0 * asin(sqrt(a));
+
+    double Result = EarthRadius * c;
+
+    return Result;
 }
 
 typedef enum Opt {
@@ -81,11 +130,12 @@ int main(int argc, char **argv)
     // Parse arguments
     int pair_count = DEFAULT_PAIR_COUNT;
     unsigned seed = jk_hash_uint32((uint32_t)time(NULL));
-    char *file_path = DEFAULT_FILE_PATH;
+    char *json_file_path = DEFAULT_JSON_FILE_PATH;
+    char *answer_file_path = DEFAULT_ANSWER_FILE_PATH;
     {
         jk_options_parse(argc, argv, opts, opt_results, OPT_COUNT, &opts_parse);
         if (opts_parse.operand_count == 1) {
-            file_path = opts_parse.operands[0];
+            json_file_path = opts_parse.operands[0];
         } else if (opts_parse.operand_count > 1 && !opt_results[OPT_HELP].present) {
             fprintf(stderr,
                     "%s: Expected 0-1 operands, got %zu\n",
@@ -123,17 +173,69 @@ int main(int argc, char **argv)
             printf("NAME\n"
                    "\thaversine_generator - generates JSON for haversine coordinate pairs\n\n"
                    "SYNOPSIS\n"
-                   "\thaversine_generator [-n PAIR_COUNT] [-s SEED] [FILE]\n\n"
+                   "\thaversine_generator [-n PAIR_COUNT] [-s SEED] [JSON_FILE] [ANSWER_FILE]\n\n"
                    "DESCRIPTION\n"
                    "\thaversine_generator generates JSON for randomly-selected haversine\n"
-                   "\tcoordinate pairs, then writes it to FILE. FILE defaults to " DEFAULT_FILE_PATH
-                   "\n\n");
+                   "\tcoordinate pairs, then writes the JSON to JSON_FILE. It also writes the\n"
+                   "\thaversine distances for each pair in binary to ANSWER_FILE. That file\n"
+                   "\twill contain one 64-bit floating point value for each pair. JSON_FILE\n"
+                   "\tdefaults to " DEFAULT_JSON_FILE_PATH
+                   ". ANSWER_FILE defaults to " DEFAULT_ANSWER_FILE_PATH "\n\n");
             jk_options_print_help(stdout, opts, OPT_COUNT);
             exit(opts_parse.usage_error);
         }
     }
 
-    printf("pair_count: %d\nseed: %u\nfile_path: %s\n", pair_count, seed, file_path);
+    srand(seed);
+
+    FILE *json_file = fopen(json_file_path, "wb");
+    if (json_file == NULL) {
+        fprintf(stderr,
+                "%s: Failed to open file '%s': %s",
+                program_name,
+                json_file_path,
+                strerror(errno));
+        exit(1);
+    }
+    FILE *answers_file = fopen(answer_file_path, "wb");
+    if (answers_file == NULL) {
+        fprintf(stderr,
+                "%s: Failed to open file '%s': %s",
+                program_name,
+                answer_file_path,
+                strerror(errno));
+        fclose(json_file);
+        exit(1);
+    }
+
+    fprintf(json_file, "{\"pairs\": [\n");
+    double sum = 0.0;
+    for (int i = 0; i < pair_count; i++) {
+        double x0 = random_between(-180.0, 180.0);
+        double y0 = random_between(-90.0, 90.0);
+        double x1 = random_between(-180.0, 180.0);
+        double y1 = random_between(-90.0, 90.0);
+        fprintf(json_file,
+                "%s    {\"x0\": %.16f, \"y0\": %.16f, \"x1\": %.16f, \"y1\": %.16f}",
+                i == 0 ? "" : ",\n",
+                x0,
+                y0,
+                x1,
+                y1);
+        double distance = reference_haversine(x0, y0, x1, y1, EARTH_RADIUS);
+        sum += distance;
+        fwrite(&distance, sizeof(double), 1, answers_file);
+    }
+    fprintf(json_file, "\n]}\n");
+
+    if (opt_results[OPT_SEED].present) {
+        printf("Seed: %s\n", opt_results[OPT_SEED].arg);
+    }
+    printf("Pair count: %d\n", pair_count);
+    printf("Expected sum: %.16f\n", sum);
+
+    fclose(answers_file);
+    fclose(json_file);
 
     return 0;
 }
