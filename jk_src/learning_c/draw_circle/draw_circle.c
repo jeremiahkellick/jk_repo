a56a7bc4dd128d79de2ac324aaaeb228989873e7
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2024-04-04 12:39:34 -0400

    Improve circle drawing algorithm

--- jk_src/learning_c/draw_circle/draw_circle.c
+++ jk_src/learning_c/draw_circle/draw_circle.c
@@ -19,27 +19,21 @@ static int int_sqrt(int n)
 
 static void draw_circle(bool (*grid)[GRID_SIZE], int r)
 {
-    int prev_y = 0;
-    int r_squared = r * r;
-    for (int x = 0; x < r + 1; x++) {
-        int x_minus_r = x - r;
-        int x_minus_r_squared = x_minus_r * x_minus_r;
-        int y = int_sqrt(r_squared - x_minus_r_squared);
+    int y = r;
+    for (int x = 0; x <= y; x++) {
+        y = int_sqrt(r * r - x * x);
 
-        int y_diff = y - prev_y;
-        for (int i = 0; i < (y_diff - 1); i++) {
-            int fill_y = prev_y + i + 1;
-            grid[r + fill_y][x] = true;
-            grid[r - fill_y][x] = true;
-            grid[r + fill_y][2 * r - x] = true;
-            grid[r - fill_y][2 * r - x] = true;
-        }
-        prev_y = y;
+        grid[r + y][r + x] = true;
+        grid[r + x][r + y] = true;
 
-        grid[r + y][x] = true;
-        grid[r - y][x] = true;
-        grid[r + y][2 * r - x] = true;
-        grid[r - y][2 * r - x] = true;
+        grid[r + y][r - x] = true;
+        grid[r + x][r - y] = true;
+
+        grid[r - y][r + x] = true;
+        grid[r - x][r + y] = true;
+
+        grid[r - y][r - x] = true;
+        grid[r - x][r - y] = true;
     }
 }
 
