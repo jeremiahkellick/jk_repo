1508da39ef17846533c55b8133f9979f5fda5af4
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2024-04-04 12:44:14 -0400

    Change draw_circle to not require floating point

--- jk_src/learning_c/draw_circle/draw_circle.c
+++ jk_src/learning_c/draw_circle/draw_circle.c
@@ -11,17 +11,37 @@
 
 #define RADIUS 15
 #define GRID_SIZE (2 * RADIUS + 1)
+#define MAX_ROOT 32
+
+static int int_sqrt_rec(int n, int min, int max)
+{
+    if (min > max) {
+        fprintf(stderr, "int_sqrt: Square root not found\n");
+        exit(1);
+    }
+    int root = min + (max - min) / 2;
+    int diff = root * root - n;
+    int next_diff = (root + 1) * (root + 1) - n;
+    if (diff <= 0 && next_diff > 0) { // Found the root because root^2 <= n and (root + 1)^2 > n
+        return -diff < next_diff ? root : root + 1;
+    } else if (diff < 0) { // Root is too low
+        return int_sqrt_rec(n, root + 1, max);
+    } else { // Root is too high
+        return int_sqrt_rec(n, min, root - 1);
+    }
+}
 
 static int int_sqrt(int n)
 {
-    return (int)sqrt((double)n);
+    return int_sqrt_rec(n, 0, MAX_ROOT);
 }
 
 static void draw_circle(bool (*grid)[GRID_SIZE], int r)
 {
     int y = r;
+    int r_squared = r * r;
     for (int x = 0; x <= y; x++) {
-        y = int_sqrt(r * r - x * x);
+        y = int_sqrt(r_squared - x * x);
 
         grid[r + y][r + x] = true;
         grid[r + x][r + y] = true;
