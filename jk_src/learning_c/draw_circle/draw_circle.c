61dc53a5b362254bafe930009fc22a1ded162eda
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2024-04-04 17:23:05 -0400

    Implement efficient DDA circle algorithm

--- jk_src/learning_c/draw_circle/draw_circle.c
+++ jk_src/learning_c/draw_circle/draw_circle.c
@@ -11,49 +11,44 @@
 
 #define RADIUS 15
 #define GRID_SIZE (2 * RADIUS + 1)
-#define MAX_ROOT 32
+#define ITERATION_COUNT 10000000
 
-static int int_sqrt_rec(int n, int min, int max)
-{
-    if (min > max) {
-        fprintf(stderr, "int_sqrt: Square root not found\n");
-        exit(1);
-    }
-    int root = min + (max - min) / 2;
-    int diff = root * root - n;
-    int next_diff = (root + 1) * (root + 1) - n;
-    if (diff <= 0 && next_diff > 0) { // Found the root because root^2 <= n and (root + 1)^2 > n
-        return -diff < next_diff ? root : root + 1;
-    } else if (diff < 0) { // Root is too low
-        return int_sqrt_rec(n, root + 1, max);
-    } else { // Root is too high
-        return int_sqrt_rec(n, min, root - 1);
-    }
-}
+static bool screen[GRID_SIZE][GRID_SIZE];
 
-static int int_sqrt(int n)
-{
-    return int_sqrt_rec(n, 0, MAX_ROOT);
-}
-
+/**
+ * Uses Casey Muratori's Efficient DDA Circle Outlines algorithm from
+ * https://www.computerenhance.com/p/efficient-dda-circle-outlines
+ */
 static void draw_circle(bool (*grid)[GRID_SIZE], int r)
 {
+    int compare = -2 * r + 1;
+    int delta_x = 2;
+    int delta_y = -4 * r + 4;
+
+    int x = 0;
     int y = r;
-    int r_squared = r * r;
-    for (int x = 0; x <= y; x++) {
-        y = int_sqrt(r_squared - x * x);
-
-        grid[r + y][r + x] = true;
+    while (x <= y) {
         grid[r + x][r + y] = true;
+        grid[r + y][r + x] = true;
 
-        grid[r + y][r - x] = true;
         grid[r + x][r - y] = true;
+        grid[r + y][r - x] = true;
 
-        grid[r - y][r + x] = true;
         grid[r - x][r + y] = true;
+        grid[r - y][r + x] = true;
 
-        grid[r - y][r - x] = true;
         grid[r - x][r - y] = true;
+        grid[r - y][r - x] = true;
+
+        x++;
+        compare += delta_x;
+        delta_x += 4;
+
+        if (compare > 0) {
+            y--;
+            compare += delta_y;
+            delta_y += 4;
+        }
     }
 }
 
@@ -69,20 +64,14 @@ static void print_grid(bool (*grid)[GRID_SIZE], int height, int width)
 
 int main(void)
 {
-    bool grid[GRID_SIZE][GRID_SIZE];
-    for (int i = 0; i < GRID_SIZE; i++) {
-        for (int j = 0; j < GRID_SIZE; j++) {
-            grid[i][j] = false;
-        }
-    }
-
+    printf("Drawing a circle %d times\n", ITERATION_COUNT);
     jk_profile_begin();
-    for (int i = 0; i < 10000000; i++) {
-        draw_circle(grid, RADIUS);
+    for (int i = 0; i < ITERATION_COUNT; i++) {
+        draw_circle(screen, RADIUS);
     }
     jk_profile_end_and_print();
 
-    print_grid(grid, GRID_SIZE, GRID_SIZE);
+    print_grid(screen, GRID_SIZE, GRID_SIZE);
 
     return 0;
 }
