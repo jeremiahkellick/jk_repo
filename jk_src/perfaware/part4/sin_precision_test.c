f875d5715badc7d611abc0e75cfd0e39287cbaf9
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2025-04-21 17:55:54 -0400

    Add unrolled sin function to precision test

--- jk_src/perfaware/part4/sin_precision_test.c
+++ jk_src/perfaware/part4/sin_precision_test.c
@@ -24,12 +24,30 @@ double compute_polynomial(double x, double *coefficients, uint64_t coefficient_c
     return _mm_cvtsd_f64(result) * x;
 }
 
+double sin_mftwp_unrolled(double x)
+{
+    double x_squared = x * x;
+
+    double result = fma(0x1.883c1c5deffbep-49, x_squared, -0x1.ae43dc9bf8ba7p-41);
+    result = fma(result, x_squared, 0x1.6123ce513b09fp-33);
+    result = fma(result, x_squared, -0x1.ae6454d960ac4p-26);
+    result = fma(result, x_squared, 0x1.71de3a52aab96p-19);
+    result = fma(result, x_squared, -0x1.a01a01a014eb6p-13);
+    result = fma(result, x_squared, 0x1.11111111110c9p-7);
+    result = fma(result, x_squared, -0x1.5555555555555p-3);
+    result = fma(result, x_squared, 0x1p0);
+
+    return result * x;
+}
+
 int main(void)
 {
     JkPrecisionTest test = {0};
 
     while (jk_precision_test(&test, -JK_PI, JK_PI, 100000000)) {
         double reference = sin(test.input);
+        jk_precision_test_result(
+                &test, reference, sin_mftwp_unrolled(test.input), "sin_mftwp_unrolled");
         for (int coefficient_count = 2; coefficient_count < JK_ARRAY_COUNT(SineRadiansC_Taylor);
                 coefficient_count++) {
             jk_precision_test_result(&test,
