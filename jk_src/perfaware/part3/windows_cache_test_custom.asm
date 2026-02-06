570014cd52f41a3413f9a6911ef5bc21790349a2
Jeremiah Kellick <jeremiah@jeremiahkellick.com>
2026-02-06 01:18:23 -0500

    Add write tests and CSV output to cache test

--- jk_src/perfaware/part3/windows_cache_test_custom.asm
+++ jk_src/perfaware/part3/windows_cache_test_custom.asm
@@ -1,4 +1,5 @@
 global read_asm_custom
+global write_asm_custom
 
 section .text
 
@@ -20,6 +21,32 @@ read_asm_custom:
         vmovdqu ymm0, [r9 + 0xa0]
         vmovdqu ymm0, [r9 + 0xe0]
         vmovdqu ymm0, [r9 + 0xc0]
+        add r9, 0x100
+        dec rax
+        jnz .inner_loop
+
+    dec rcx
+    jnz .outer_loop
+    ret
+
+; arg1 (rcx): Outer loop iterations
+; arg2 (rdx): Inner loop iterations
+; arg3  (r8): Buffer data pointer
+write_asm_custom:
+    align 64
+.outer_loop:
+    mov rax, rdx
+    mov r9, r8
+
+    .inner_loop:
+        vmovdqu [r9], ymm0
+        vmovdqu [r9 + 0x20], ymm0
+        vmovdqu [r9 + 0x40], ymm0
+        vmovdqu [r9 + 0x60], ymm0
+        vmovdqu [r9 + 0x80], ymm0
+        vmovdqu [r9 + 0xa0], ymm0
+        vmovdqu [r9 + 0xe0], ymm0
+        vmovdqu [r9 + 0xc0], ymm0
         add r9, 0x100
         dec rax
         jnz .inner_loop
--- jk_src/perfaware/part3/windows_cache_test_custom.c
+++ jk_src/perfaware/part3/windows_cache_test_custom.c
@@ -11,6 +11,7 @@
 // #jk_build dependencies_end
 
 void read_asm_custom(int64_t outer_loop_iterations, int64_t inner_loop_iterations, void *data);
+void write_asm_custom(int64_t outer_loop_iterations, int64_t inner_loop_iterations, void *data);
 
 static int64_t sizes[] = {
     // L1 boundary
@@ -23,13 +24,19 @@ static int64_t sizes[] = {
     48 * 1024,
 
     // L2 boundary
-    248 * 1024,
-    252 * 1024,
+    160 * 1024,
+    176 * 1024,
+    192 * 1024,
+    208 * 1024,
+    224 * 1024,
+    240 * 1024,
     256 * 1024,
-    260 * 1024,
-    264 * 1024,
-    268 * 1024,
     272 * 1024,
+    288 * 1024,
+    304 * 1024,
+    320 * 1024,
+    336 * 1024,
+    352 * 1024,
 
     // L3 boundary
     2 * 1024 * 1024,
@@ -55,7 +62,17 @@ static int64_t sizes[] = {
     1 * 1024 * 1024 * 1024,
 };
 
-static JkPlatformRepetitionTest tests[JK_ARRAY_COUNT(sizes)];
+typedef struct TestFunction {
+    char *name;
+    void (*call)(int64_t outer_loop_iterations, int64_t inner_loop_iterations, void *data);
+} TestFunction;
+
+TestFunction functions[] = {
+    {"Read", read_asm_custom},
+    {"Write", write_asm_custom},
+};
+
+static JkPlatformRepetitionTest tests[JK_ARRAY_COUNT(sizes)][JK_ARRAY_COUNT(functions)];
 
 #define BUFFER_SIZE (sizes[JK_ARRAY_COUNT(sizes) - 1])
 
@@ -65,25 +82,22 @@ int32_t jk_platform_entry_point(int32_t argc, char **argv)
 
     JkBuffer buffer = jk_platform_memory_alloc(JK_ALLOC_COMMIT, BUFFER_SIZE);
 
-    for (;;) {
-        for (int64_t i = 0; i < JK_ARRAY_COUNT(tests); i++) {
-            JkPlatformRepetitionTest *test = &tests[i];
+    for (int64_t size_index = 0; size_index < JK_ARRAY_COUNT(tests); size_index++) {
+        for (int64_t func_index = 0; func_index < JK_ARRAY_COUNT(functions); func_index++) {
+            JkPlatformRepetitionTest *test = &tests[size_index][func_index];
 
-            if (sizes[i] < 1024 * 1024) {
-                printf("\n%.2f KiB\n", (double)sizes[i] / 1024.0);
-            } else if (sizes[i] < 1024 * 1024 * 1024) {
-                printf("\n%.2f MiB\n", (double)sizes[i] / (1024.0 * 1024.0));
-            } else {
-                printf("\n%.2f GiB\n", (double)sizes[i] / (1024 * 1024.0 * 1024.0));
-            }
+            printf("\n");
+            jk_platform_print_bytes_int64(stdout, "%.0f", sizes[size_index]);
+            printf(", %s\n", functions[func_index].name);
 
-            int64_t inner_loop_iterations = sizes[i] / 256;
-            int64_t outer_loop_iterations = BUFFER_SIZE / sizes[i];
-            int64_t byte_count = outer_loop_iterations * sizes[i];
-            jk_platform_repetition_test_run_wave(test, byte_count, frequency, 10);
+            int64_t inner_loop_iterations = sizes[size_index] / 256;
+            int64_t outer_loop_iterations = BUFFER_SIZE / sizes[size_index];
+            int64_t byte_count = outer_loop_iterations * sizes[size_index];
+            jk_platform_repetition_test_run_wave(test, byte_count, frequency, 5);
             while (jk_platform_repetition_test_running(test)) {
                 jk_platform_repetition_test_time_begin(test);
-                read_asm_custom(outer_loop_iterations, inner_loop_iterations, buffer.data);
+                functions[func_index].call(
+                        outer_loop_iterations, inner_loop_iterations, buffer.data);
                 jk_platform_repetition_test_time_end(test);
                 jk_platform_repetition_test_count_bytes(test, byte_count);
             }
@@ -92,6 +106,22 @@ int32_t jk_platform_entry_point(int32_t argc, char **argv)
                 exit(1);
             }
         }
+    }
+
+    printf("\nSize");
+    for (int64_t i = 0; i < JK_ARRAY_COUNT(functions); i++) {
+        printf(",%s", functions[i].name);
+    }
+    printf("\n");
+    for (int64_t size_index = 0; size_index < JK_ARRAY_COUNT(tests); size_index++) {
+        jk_platform_print_bytes_int64(stdout, "%.0f", sizes[size_index]);
+        for (int64_t func_index = 0; func_index < JK_ARRAY_COUNT(functions); func_index++) {
+            printf(",%.3f",
+                    jk_platform_repetition_test_bandwidth(
+                            tests[size_index][func_index].min, frequency)
+                            / (1024.0 * 1024.0 * 1024.0));
+        }
+        printf("\n");
     }
 
     return 0;
